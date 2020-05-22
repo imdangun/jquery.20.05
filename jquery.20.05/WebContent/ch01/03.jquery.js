5b4dbabc1c5999d99f9297efda28998660557cfb
SIST98 <SIST98@DESKTOP-VSOFP86>
2020-05-22 14:09:02 +0900

    a

---
+++ jquery.20.05/WebContent/ch01/03.jquery.js
@@ -0,0 +1,114 @@
+/** $('css selector') **/
+
+$('#myId')  // <div id='myId'></div>  
+
+$('.myClass')  // <div class='myClass'></div>
+
+$('input[name="surname"]') // <input name='surname'/>
+
+$('#contents ul.people li')
+/*
+<div id='contents'>
+	<ul class='people'>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+*/
+
+$('a.external:first')
+/*
+<a class='external'></a> --
+<a class='external'></a>
+*/
+
+$('tr:odd')
+/*
+<tr></tr> --
+<tr></tr>
+<tr></tr> --
+*/
+
+$('div:visible')
+
+$('div:gt(1)') // greater than
+/*
+<div></div>
+<div></div>
+<div></div> --
+*/
+
+$('div:animated')
+
+$('a[rel$="thinger"]')  // <a rel='do-nothinger'></a>
+
+//
+if($('div.foo')) {}
+if($('div.foo').length) {}
+
+
+/** filtering **/
+$('div.foo').has('p')
+$('ul li').filter('.current')
+$('ul li').not('.current')
+$('ul li').eq(5)
+
+
+/** form **/
+$('form :button')
+/*
+<form>
+	<button></button>     --
+	<input type='button'> --
+</form>
+*/
+
+$('form :radio')     // <input type='radio'/>
+$('form :checkbox')  // <input type='checkbox'/>
+
+$('form :checked')
+/*
+<input type='checkbox'/>
+<input type='radio'/>
+<select>
+*/
+
+$('from :selected')  // <select><option></option></select>
+
+$('form :enabled')
+$('form :disabled')
+
+$('form :file') // <input type='file'/>
+
+$('form :input')
+/*
+<input type='text'/>
+<textarea></textarea>
+<select></select>
+<button type='submit'></button>
+*/
+
+$('form :text')   // <input type='text'/>
+
+$('form :password') // <input type='password'/>
+
+$('form input[name="gender"]:radio') // <input type='radio' name='gender'/>
+
+$('form :reset')  // <input type='reset'/>
+
+$('form :submit') // <input type='submit'/>
+
+/*
+- .has(selector): 
+  Reduce the set of matched elements to those that have a descendant 
+  that matches the selector or DOM element.
+
+- .not(selector):
+  Remove elements from the set of matched elements.
+
+- .filter(selector):
+  Reduce the set of matched elements to those that match the selector.
+
+- .eq(index):
+  Reduce the set of matched elements to the one at the specified index.
+*/
